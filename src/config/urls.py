43db4a780d12b02a191620cb09307fe907bf81fa
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-02-07 12:34:51 +0300

    Create the main django application with a single view
    now, if you go to <local host:8000/>, you will see the phrase <hello, world! this is the app!>.

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
 
-urlpatterns = [
+urlpatterns = (
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
-]
+)
---
+++ src/main/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Hello, World! this is application!')
