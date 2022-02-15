d6f6c008128f6548e7611a3ab71ea620b7494f75
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-02-15 14:21:16 +0300

    Fix some errors in the project settings which identified by linters

--- server/settings/components/__init__.py
+++ server/settings/components/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
