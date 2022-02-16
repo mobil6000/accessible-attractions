bc14b38340ac21ca56ea29c29fb9b4cd6a6e0306
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-02-16 14:06:59 +0300

    Add a model describing the attraction

--- server/apps/main/models.py
+++ server/apps/main/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from typing import final
 
-# Create your models here.
+from django.db import models
+
+
+
+@final
+class Attraction(models.Model):
+    name = models.CharField(max_length=128, unique=True, verbose_name='Название достопримечательности')
+    description = models.TextField(verbose_name='Описание')
+    route = models.TextField(verbose_name='Маршрут до объекта')
+    return_route = models.TextField(verbose_name='Обратный маршрут')
+
+
+    class Meta:
+        db_table = 'attractions'
+        verbose_name = 'Достопримечательность'
+        verbose_name_plural = 'Достопримечательности'
+
+
+    def __str__(self) -> str:
+        return 'Attraction <{0}>'.format(self.name)
