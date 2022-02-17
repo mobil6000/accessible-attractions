d69ce8aae5438879d5c8f0521da9eb7a05ea4142
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-02-17 09:08:52 +0300

    Implement displaying the <attraction> model on the admin panel

--- server/apps/main/admin.py
+++ server/apps/main/admin.py
@@ -1,3 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from server.apps.main.models import Attraction
+
+
+
+@admin.register(Attraction)
+class AttractionAdmin(admin.ModelAdmin[Attraction]):
+    '''Admin panel object for ``Attraction`` model.'''
+    pass
