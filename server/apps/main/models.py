3701e831d5e9a1c1f53c3cbec9bd4c4dadfdc647
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-02-23 09:39:43 +0300

    Implement a django model for metro stations close to attractions

--- server/apps/main/models.py
+++ server/apps/main/models.py
@@ -9,8 +9,6 @@ class Attraction(models.Model):
     name = models.CharField(max_length=128, unique=True, verbose_name='Название')
     short_info = models.CharField(max_length=256, unique=True, verbose_name='краткое описание')
     description = models.TextField(verbose_name='Описание')
-    route = models.TextField(verbose_name='Маршрут до объекта')
-    return_route = models.TextField(verbose_name='Обратный маршрут')
 
 
     class Meta:
@@ -21,3 +19,26 @@ class Attraction(models.Model):
 
     def __str__(self) -> str:
         return 'Attraction <{0}>'.format(self.name)
+
+
+
+@final
+class MetroStation(models.Model):
+    attraction = models.ForeignKey(
+        to=Attraction,
+        on_delete=models.CASCADE,
+        related_name='nearest_metro_stations'
+    )
+    station_name = models.CharField(max_length=80, unique=True, verbose_name='Название станции')
+    route_from_station = models.TextField(verbose_name='Маршрут от станции до объекта')
+    route_to_station = models.TextField(verbose_name='Маршрут от объекта до станции')
+
+
+    class Meta:
+        db_table = 'metro_stations'
+        verbose_name = 'Станция метро'
+        verbose_name_plural = 'Станции метро'
+
+
+    def __str__(self) -> str:
+        return 'MetroStation:<{0}>'.format(self.station_name)
