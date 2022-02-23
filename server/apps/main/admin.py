d1c40b861d5e7f422c9eb980844b11c1a26890fe
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-02-23 17:41:56 +0300

    Register a model describing a metro station in the administrative panel

--- server/apps/main/admin.py
+++ server/apps/main/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 
-from server.apps.main.models import Attraction
+from server.apps.main.models import Attraction, MetroStation
 
+
+class MetroStationInline(admin.TabularInline):
+    model = MetroStation
 
 
 @admin.register(Attraction)
 class AttractionAdmin(admin.ModelAdmin[Attraction]):
     '''Admin panel object for ``Attraction`` model.'''
+    inlines = (MetroStationInline,)
+
+
+@admin.register(MetroStation)
+class MetroStationAdmin(admin.ModelAdmin[Attraction]):
+    '''Admin panel object for ``MetroStation`` model.'''
     pass
