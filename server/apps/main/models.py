df2cb45d50f4ac4038239fc0d2c45e79684211f9
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-02-25 16:19:27 +0300

    Add the option to select the type of metro station

--- server/apps/main/models.py
+++ server/apps/main/models.py
@@ -24,12 +24,23 @@ class Attraction(models.Model):
 
 @final
 class MetroStation(models.Model):
+
+    class StationTypeVariants(models.TextChoices):
+        METRO = ('m', 'станция метро')
+        MCC = ('mcc', 'станция МЦК')
+
     attraction = models.ForeignKey(
         to=Attraction,
         on_delete=models.CASCADE,
         related_name='nearest_metro_stations'
     )
     station_name = models.CharField(max_length=80, unique=True, verbose_name='Название станции')
+    station_type = models.CharField(
+        max_length=3,
+        choices=StationTypeVariants.choices,
+        default=StationTypeVariants.METRO,
+        verbose_name='Тип станции'
+    )
     route_from_station = models.TextField(verbose_name='Маршрут от станции до объекта')
     route_to_station = models.TextField(verbose_name='Маршрут от объекта до станции')
 
