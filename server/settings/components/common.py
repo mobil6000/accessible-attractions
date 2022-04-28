531017c7c4e08db6832beab73a6f034b8348ff31
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-04-28 09:20:41 +0300

    Change a password heshing algorithm.
    now it Implements by <bcrypt< library

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -47,6 +47,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
+
 
 ROOT_URLCONF = 'server.urls'
 WSGI_APPLICATION = 'server.wsgi.application'
