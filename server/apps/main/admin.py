91e4ea1ec750586ebd19b8272ad06a10d1b10e28
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-05-25 21:11:09 +0300

    Implement correct view of >AboutUsPage> model in the admin panel

--- server/apps/main/admin.py
+++ server/apps/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.http import HttpRequest
 
-from .models import Attraction, MetroStation, Photo
+from .models import AboutUsPage, Attraction, MetroStation, Photo
 
 
 
@@ -9,13 +10,27 @@ class PhotoInline(admin.StackedInline[Photo]):
     extra = 1
 
 
+
 @admin.register(Attraction)
 class AttractionAdmin(admin.ModelAdmin[Attraction]):
     '''Admin panel object for ``Attraction`` model.'''
     inlines = (PhotoInline,)
 
 
+
 @admin.register(MetroStation)
-class MetroStationAdmin(admin.ModelAdmin[Attraction]):
+class MetroStationAdmin(admin.ModelAdmin[MetroStation]):
     '''Admin panel object for ``MetroStation`` model.'''
     pass
+
+
+
+@admin.register(AboutUsPage)
+class AboutUsPageAdmin(admin.ModelAdmin[AboutUsPage]):
+    '''Admin panel object for ``AboutUsPage`` model.'''
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        if self.model.objects.count() > 0:
+            return False
+        else:
+            return super().has_add_permission(request)
