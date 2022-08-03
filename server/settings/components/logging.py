4ebb8076e1562acd7ec98c1a2b7e27c6bcdc0e19
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-08-03 06:44:52 +0300

    Make logging configurations more human readable

--- server/settings/components/logging.py
+++ server/settings/components/logging.py
@@ -1,42 +1,51 @@
-# Future logging configuration
+'''
+Configuring the logging mechanism for the application
+'''
 
 
 
-_LOG_MSG_FORMAT = {'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:(lineno)d]'}
+_LOG_MESSAGE_FORMATTERS = {
+    'default': {'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:(lineno)d]'},
+}
+
+
+_LOG_HENDLERS = {
+    'info_console_handler': {
+        'class': 'logging.StreamHandler',
+        'formatter': 'default',
+        'stream': 'ext://sys.stdout',
+    },
+    'error_console_handler': {
+        'class': 'logging.StreamHandler',
+        'formatter': 'default',
+        'stream': 'ext://sys.stderr',
+    },
+}
+
+
+_LOGGERS = {
+    'django': {
+        'handlers': ['info_console_handler'],
+        'propagate': True,
+        'level': 'INFO',
+    },
+    'security': {
+        'handlers': ['error_console_handler'],
+        'level': 'ERROR',
+        'propagate': False,
+    },
+    'django.db.backends': {
+        'handlers': ['info_console_handler'],
+        'level': 'DEBUG',
+        'propagate': False,
+    },
+}
+
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {'default': _LOG_MSG_FORMAT},
-
-    'handlers': {
-        'info_console_handler': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'default',
-            'stream': 'ext://sys.stdout',
-        },
-        'error_console_handler': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'default',
-            'stream': 'ext://sys.stderr',
-        },
-    },
-
-    'loggers': {
-        'django': {
-            'handlers': ['info_console_handler'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        'security': {
-            'handlers': ['error_console_handler'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        'django.db.backends': {
-            'handlers': ['info_console_handler'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
+    'formatters': _LOG_MESSAGE_FORMATTERS,
+    'handlers': _LOG_HENDLERS,
+    'loggers': _LOGGERS,
 }
