eb27534aa98a117a3f61fc3a5d51fed700cb2a62
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-10-05 11:47:23 +0300

    Implement displaying page with search results

--- server/apps/main/urls.py
+++ server/apps/main/urls.py
@@ -5,13 +5,15 @@ from .views import (
     show_attraction_detail,
     show_attractions_list,
     show_help,
-    show_routes_for_attraction
+    show_routes_for_attraction,
+    show_search_results,
 )
 
 
 
 urlpatterns = (
     path('', index, name='index'),
+    path('search/', show_search_results, name='search'),
     path('about/', show_help, name='about'),
     path('attractions/', show_attractions_list, name='attractions_list'),
     path('attractions<int:attraction_id>/', show_attraction_detail, name='attraction_detail'),
--- server/apps/main/views.py
+++ server/apps/main/views.py
@@ -6,7 +6,8 @@ from .services import (
     get_about_site_info,
     get_attraction_detail,
     get_attraction_previews,
-    get_metro_stations_for_attraction
+    get_metro_stations_for_attraction,
+    search_attractions
 )
 
 
@@ -39,6 +40,15 @@ def show_routes_for_attraction(request: HttpRequest, attraction_id: int) -> Http
         raise Http404('error!')
     response_context = {'content': result}
     return render(request, 'main/routes.html', response_context)
+
+
+def show_search_results(request: HttpRequest) -> HttpResponse:
+    search_query = request.GET.get('q')
+    try:
+        search_results = search_attractions(search_query)
+    except BusinessLogicFailure:
+        raise Http404('error!')
+    return render(request, 'main/search_results.html', {'content': search_results})
 
 
 def show_help(request: HttpRequest) -> HttpResponse:
